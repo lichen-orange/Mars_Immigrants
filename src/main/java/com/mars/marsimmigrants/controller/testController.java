6f8e23d53f593f4317a4c97f712dc41018f927ad
李陈 <5236566+kgc_li_chen@user.noreply.gitee.com>
2020-01-14 17:14:28 +0800

    李陈第三次提交

---
+++ src/main/java/com/mars/marsimmigrants/controller/testController.java
@@ -0,0 +1,11 @@
+package com.mars.marsimmigrants.controller;
+
+/**
+ * @author 李陈
+ * @version V1.0
+ * @Project: mars_immigrants
+ * @Package com.mars.marsimmigrants.controller
+ * @date 2020/1/14 17:05 星期二
+ */
+public class testController {
+}
---
+++ src/main/java/com/mars/marsimmigrants/mapper/tsetDao.java
@@ -0,0 +1,11 @@
+package com.mars.marsimmigrants.mapper;
+
+/**
+ * @author 李陈
+ * @version V1.0
+ * @Project: mars_immigrants
+ * @Package com.mars.marsimmigrants.mapper
+ * @date 2020/1/14 17:05 星期二
+ */
+public class tsetDao {
+}
